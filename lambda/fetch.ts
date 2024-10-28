b844af07c3706bc152f2c0df800fa3c4fed52b79
zoror901 <zoro907@gmail.com>
2024-10-29 00:01:45 +0900

    fetch.tsをurlを連結させる動作に変更

--- lambda/fetch.ts
+++ lambda/fetch.ts
@@ -1,25 +1,35 @@
-import type {
-  APIGatewayEvent,
-  APIGatewayProxyResult,
-  Handler,
-} from 'aws-lambda'
+import type { APIGatewayProxyResult, Context, Handler } from 'aws-lambda'
+import type { APIGatewayProxyEventV2WithRequestContext } from 'aws-lambda/trigger/api-gateway-proxy'
 
-export const handler: Handler<APIGatewayEvent, APIGatewayProxyResult> = async (
-  event,
-  context,
-) => {
-  try {
-    const { url, method } = JSON.parse(event.body || '{}')
-    const json = await fetch(url, { method: method ?? 'GET' })
-      .then(response => response.json())
-    return {
-      statusCode: 200,
-      body: JSON.stringify(json),
-    }
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: 'failed',
-    }
-  }
+export const handler: Handler<
+	APIGatewayProxyEventV2WithRequestContext<Context>,
+	APIGatewayProxyResult
+> = async (event) => {
+	try {
+		const response = await fetch(
+			`${event.rawPath.replace('/', '')}?${event.rawQueryString}`,
+		)
+		const json = await response.json()
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(json),
+		}
+	} catch (e) {
+		console.error(e)
+		return {
+			statusCode: 500,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				message: 'failed',
+			}),
+		}
+	}
 }
